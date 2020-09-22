f046734651a4b6caf68b41bc73f8baf27a9832ce
Elad Segal <eladsegal@users.noreply.github.com>
2020-09-22 18:11:53 +0300

    Load to cpu when using PretrainedModelInitializer

--- allennlp/nn/initializers.py
+++ allennlp/nn/initializers.py
@@ -405,7 +405,7 @@ class PretrainedModelInitializer(Initializer):
 
             weights_file_path = os.path.join(tempdir, _WEIGHTS_NAME)
 
-        self.weights: Dict[str, torch.Tensor] = torch.load(weights_file_path)
+        self.weights: Dict[str, torch.Tensor] = torch.load(weights_file_path, map_location="cpu")
         self.parameter_name_overrides = parameter_name_overrides or {}
 
     @overrides
